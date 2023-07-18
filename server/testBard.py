f94867f94043d6fe32c90c72bca03a4b57dd53ca
Trịnh Quang Trường <103560993+JoshuaTrinh5262@users.noreply.github.com>
2023-07-18 15:34:18 +0700

    fix testbard.py

--- server/testBard.py
+++ server/testBard.py
@@ -3,6 +3,7 @@ import requests
 import dotenv
 import csv
 import os
+
 config = dotenv.dotenv_values("./.env")
 bard_token = config['BARD_TOKEN']
 
@@ -21,8 +22,8 @@ session.headers = {
             "X-Same-Domain": "1",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-            "Origin": " https://bard.google.com ",
-            "Referer": " https://bard.google.com/ ",
+            "Origin": "https://bard.google.com",
+            "Referer": "https://bard.google.com/",
         }
 session.cookies.set("__Secure-1PSID", bard_token) 
 bard = Bard(token = bard_token, session = session, timeout = 30)
